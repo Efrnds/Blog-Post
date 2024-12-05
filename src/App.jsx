ea942b4b27ef8242ae95c6abb6640acb882bbe4a
efrnds <eduardo.fernandes.silva13@gmail.com>
2024-12-05 19:42:44 -0300

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,24 @@ import { useState } from "react";
 import About from "./About";
 import Login from "./Login";
 import Home from "./home";
+import Sidebar from "./Sidebar";
 
 export default function App() {
   const [isLogin, setIsLogin] = useState(false);
 
   return (
     <BrowserRouter>
+      {isLogin && <Sidebar />}
       <Routes>
         <Route
           path="/"
-          element={
-            isLogin ? (
-              <Home />
-            ) : (
-              <Login setIsLogin={setIsLogin} />
-            )
-          }
+          element={isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}
         />
         <Route
           path="/about"
-          element={
-            isLogin ? (
-              <About />
-            ) : (
-              <Login setIsLogin={setIsLogin} />
-            )
-          }
+          element={isLogin ? <About /> : <Login setIsLogin={setIsLogin} />}
         />
-        <Route
-          path="/login"
-          element={<Login setIsLogin={setIsLogin} />}
-        />
+        <Route path="/login" element={<Login setIsLogin={setIsLogin} />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Sidebar.jsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Menu</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
